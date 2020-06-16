c900bc7002678559f220309db9465d99f6d4e995
kostanzhoglo <jeffreywithers@gmail.com>
2020-06-16 15:44:06 -0400

    Pull data from db through views.py and push with formatting thru to templates, and format questions in index.html

--- nba_teams_polls/polls/views.py
+++ nba_teams_polls/polls/views.py
@@ -4,6 +4,8 @@ from .models import Question, Choice
 
 # Get questions and display them
 def index(request):
-    return render(request, 'polls/index.html')  # index.html is a Template we will make... and we ALSO haven't created a URL for that Template yet.
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)  # index.html is a Template we will make... and we ALSO haven't created a URL for that Template yet.
     
 
