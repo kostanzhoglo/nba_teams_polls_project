8d85c5c3c79fb2568afd6f3e8bb4d543a5e2add8
kostanzhoglo <jeffreywithers@gmail.com>
2020-06-17 15:41:58 -0400

    Add vote method to views.py, add django imports to views.py, and add vote url to polls/urls.py

--- nba_teams_polls/polls/urls.py
+++ nba_teams_polls/polls/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     # next line is creating a route
     path('', views.index, name='index'),
     path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results', views.results, name='results')
+    path('<int:question_id>/results', views.results, name='results'),
+    path('<int:question_id>/vote', views.vote, name='vote')
 ]
